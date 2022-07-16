64e9bf7a1f77d3b59d7de61d49e251f62d672b10
diogodvs <dvsdevx@gmail.com>
2022-07-16 10:03:39 -0300

    adjust folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './componets/template/nav-bar/nav-bar.component';
-import { AboutComponent } from './componets/template/about/about.component';
-import { MainHomeComponent } from './componets/template/main-home/main-home.component';
-import { CarouselComponent } from './componets/template/carousel/carousel.component';
-import { ValuesComponent } from './componets/template/values/values.component';
-import { WhyChooseComponent } from './componets/template/why-choose/why-choose.component';
+import { NavBarComponent } from './componets/nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AboutComponent,
-    MainHomeComponent,
-    CarouselComponent,
-    ValuesComponent,
-    WhyChooseComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
